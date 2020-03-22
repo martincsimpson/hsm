e0aaa67fe7efa9c7db8645a2eb0beab8746a3969
root <root@localhost.localdomain>
2020-03-22 04:26:37 -0400

    failing test for library source
    next step, working test for library source

--- spec.rb
+++ spec.rb
@@ -1,16 +1,17 @@
-class LibraryRepository
-  def all
-    [Library.new]
-  end
-end
-
 class Library
 end
 
 describe "LibraryRepository" do
   context 'get all libraries' do
+    
     it 'should return a library' do
       # given
+      class LibraryRepository
+        def all
+          [Library.new]
+        end
+      end
+      
       repository = LibraryRepository.new
     
       # when
@@ -19,5 +20,29 @@ describe "LibraryRepository" do
       # then
       expect(result.first).to be_instance_of(Library)
     end
+    
+    it 'should return a library from github' do
+      # given
+      source = double("GitHubLibrarySource", name: :github, fetch: [Library.new])
+      
+      class LibraryRepository
+        def initialize source:
+          @source = source
+        end
+        
+        def all
+          @source.fetch
+        end
+      end
+      
+      repository = LibraryRepository.new(source: source)
+
+      # when
+      result = repository.all
+    
+      # then
+      expect(result.first.source).to eq(:github)
+    end
+    
   end
 end
