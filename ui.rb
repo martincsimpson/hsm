bbbad30f38b4b360375f22d491b4becdfc7ee94d
Martin Simpson <martinsimpson@Martins-MacBook-Pro.local>
2020-03-23 00:27:26 +0700

    missing timeout error was added to sinatra ui

--- ui.rb
+++ ui.rb
@@ -15,7 +15,11 @@ set :bind, '0.0.0.0'
 get '/libraries/:language' do
   library_repository = Library::Repository.new(sources: [GitHubSource.new, GitLabSource.new])
   
-  Cache.get_or_set(params[:language]) do
-    JSON.dump(library_repository.all(language: params[:language]))
+  begin
+    Cache.get_or_set(params[:language]) do
+      JSON.dump(library_repository.all(language: params[:language]))
+    end
+  rescue Timeout::Error => e
+    JSON.dump({ error: "TimeoutErrorFromBackend", message: "One or more of the resource backends have timed out"})
   end
 end
